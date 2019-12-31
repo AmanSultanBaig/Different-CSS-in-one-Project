11fbcc70e59bfd534286fe7bd78fd3bef5602b2c
Aman Sultan Baig <amansultanbaig@gmail.com>
2019-12-30 21:28:15 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+ // this approach for applying css according to current language e.i english, french, arabic
   var lang = localStorage.getItem('lang');
         if (lang === "ARB") {
             document.write("<link rel='stylesheet' href='assets/css/customCSS_AR.css' type='text/css' media='all'/>");
@@ -7,7 +8,7 @@
             document.write("<link rel='stylesheet' href='assets/css/customCSS.css' type='text/css' media='all'/>");
         }
 
-
+ // this approach is for page postion according to current language e.i english, french, arabic 
   jQuery(document).ready(function () {
         var lang = localStorage.getItem('lang');
         if (lang === 'ARB') {
